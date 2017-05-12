bfb9efb7b16a5444239130a2421c6071b31e8138
hantongliu <leonliu1989@hotmail.com>
2017-05-12 15:17:15 +0200

    some animals

---
+++ MyName/src/Dog.java
@@ -0,0 +1,9 @@
+
+public class Dog extends Animal {
+
+	/*public Dog() {
+	}*/
+	public void makeSound() {
+		System.out.println("wang wang!");
+	}
+}
--- MyName/src/MyName.java
+++ MyName/src/MyName.java
@@ -4,6 +4,16 @@ public class MyName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My name is Hantong.");
+		Animal a1 = new Dog();
+		Animal a2 = new Cat();
+		Animal a3 = new Animal();
+		Animal[] animals = new Animal[3];
+		animals[0] = a1;
+		animals[1] = a2;
+		animals[2] = a3;
+		for (Animal a:animals) {
+			a.makeSound();
+		}
 	}
 
 }
